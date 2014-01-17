b7ed30e19a70dbbff64db2793d304f1776b32d4d
baptistelemoine <baptiste.lemoine@gmail.com>
2014-01-17 10:16:32 +0100

    looking for typo changes, save logic

--- app/js/controllers/ListController.js
+++ app/js/controllers/ListController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controllers.controller('ListController', ['$scope', '$location', 'SearchManager', 'ConfigManager', function ($scope, $location, SearchManager, ConfigManager){
+app.controllers.controller('ListController', ['$scope', '$location', 'SearchManager', 'ConfigManager', '_', function ($scope, $location, SearchManager, ConfigManager, _){
 	
 	ConfigManager.fields = ['title,date,summary,origin','id'];
 	$scope.config = ConfigManager;
@@ -8,5 +8,18 @@ app.controllers.controller('ListController', ['$scope', '$location', 'SearchMana
 
 	$scope.search = SearchManager;
 	$scope.search.nextPage($location.$$url, true, {'sort':'desc'});
+
+	$scope.onTypoSelect = function(typo, article){
+		$scope.save(_.extend(article, {'typo':typo.val}));
+	};
+
+	$scope.onAdd = function(article){
+		$scope.save(_.extend(article, {'typo':$scope.typo.val}));
+	};
+
+	$scope.save = function(article){
+		//rest save logic here
+		console.log(article);
+	};
 
 }]);
