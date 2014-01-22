72523b6d607b108174b1155376009d5f7b571103
baptistelemoine <baptiste.lemoine@gmail.com>
2014-01-22 16:34:55 +0100

    parsing to json ok w/ promises

--- src/test.js
+++ src/test.js
@@ -4,23 +4,32 @@ var xml2js = require('xml2js');
 var Q = require('q');
 var _ = require('underscore');
 
-var fileParser = function (fileName){
+var fileParser = function (fileName, obj){
 
 	return Q.nfcall(fs.readFile, fileName)
 	.then(function (data){
-		return data.toString().replace("\ufeff", "");
+		data = data.toString().replace("\ufeff", "");
+		if(obj) {
+			obj['items'] = data;
+			return obj;
+		}
+		return data;
 	})
 	.fail(function (error){
 		console.log(error);
 	});
 };
 
-var xmlParser = function (data){
+var xmlParser = function (data, obj){
 	var q = Q.defer();
 	var parser = new xml2js.Parser({explicitArray:false, ignoreAttrs:true, normalize:true});
 	parser.parseString(data, function (err, result){
 		if(err) q.resolve(err);
-		q.resolve(result);
+		if(obj){
+			obj['items'] = result;
+			q.resolve(obj);
+		}
+		else q.resolve(result);
 	});
 	return q.promise;
 };
@@ -42,15 +51,27 @@ var rootFile = fileParser('./data/menu_config/root.xml')
 
 rootFile.then(function (result){
 	var arr = [];
-	_(result).each(function (val){
-		arr.push(fileParser(val.fileName));
+	_(result).map(function (val){
+		arr.push(fileParser(val.fileName, val));
 	});
 	return Q.all(arr);
 })
 .then(function (result){
-	console.log(result);
-});
-
+	var arr = [];
+	_(result).map(function (val){
+		arr.push(xmlParser(val.items, val));
+	});
+	return Q.all(arr);
+})
+.then(function (data){
+	return _(data).map(function (obj){
+		obj.items = JSON.stringify(obj.items.root);
+		return obj;
+	});
+})
+.done(function (result){
+	console.log(result.length)
+})
 
 
 
